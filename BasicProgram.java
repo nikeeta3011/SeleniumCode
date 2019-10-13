530ecfe60e47cc6188125213fa8f461b19dd8aae
Nikeeta Shelar <nikeeta3011@gmail.com>
2019-10-13 18:25:21 +0530

    this is the first message

---
+++ BasicProgram.java
@@ -0,0 +1,23 @@
+package session;
+
+public class BasicProgram {
+//BasicProgram obj= new BasicProgram();
+//NonStatic obj2=new NonStatic();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		//System.out.println("This is the first session");
+		
+		//BasicProgram.method();
+		//NonStatic obj=new NonStatic();
+		//obj2.abc();
+		//obj.method();
+	}
+	public int method()
+	{
+
+		System.out.println("nikeeta is the name of girl");
+	return 0;
+	}
+}
---
+++ NonStatic.java
@@ -0,0 +1,10 @@
+package session;
+
+public class NonStatic {
+	public static void abc()
+	{
+//System.out.println("System out");
+System.out.println("SYSOUT");
+
+}
+}
